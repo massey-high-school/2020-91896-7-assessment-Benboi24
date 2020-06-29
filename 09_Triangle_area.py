06ced08ad99e389637ad8a433b26b9db1e5d6dc1
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2020-06-29 12:43:20 +1200

    09_triangle_area
    This works perfectly. Dosen't have units but works great.

--- 09_Triangle_area.py
+++ 09_Triangle_area.py
@@ -16,15 +16,9 @@ def not_blank(question):
 
 # Main Rountine goes here
 
-def main():
-
 a = int(input('Enter First side'))
 b = int(input('Enter second side'))
 c = int(input('Enter Third side'))
-
-# Fiding area of a Triangle
-width = float(input('Please enter the widith of your Triangle: '))
-height = float(input('Please enter the height of your Triangle: '))
 
 # Caculate the area
 area = a + b + c/2
@@ -32,6 +26,5 @@ area = a + b + c/2
 # Calculate the perimeter
 perimeter = a + b + c
 
-print("\n Area of your Triangle is: %.2f" %Area)
-print(" Perimeter of your Triangle is: %2f" %Perimeter)
-
+print("\n Area of your Triangle is: %.2f" %area)
+print(" Perimeter of your Triangle is: %2f" %perimeter)
