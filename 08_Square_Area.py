b456dce73c5877a972c88a4d1fd3252dfca7a55a
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2020-07-20 12:58:20 +1200

    08_Square_Area
    Works good. But only needs to ask one side.

--- 08_Square_Area.py
+++ 08_Square_Area.py
@@ -16,14 +16,13 @@ def not_blank(question):
 
 # Main Rountine goes here
 # Fiding area of a square
-width = float(input('Please enter the widith of your Square: '))
-height = float(input('Please enter the height of your Square: '))
+a = float(input('Please enter side a: '))
 
 # Caculate the area
-area = height ** 2
+area = a ** 2
 
 # Calculate the perimeter
-perimeter = 4 * height
+perimeter = 4 * a
 
 print("\n Area of your square is: %.2f" %area)
 print(" Perimeter of your square is: %2f" %perimeter)
