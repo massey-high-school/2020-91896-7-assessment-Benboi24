db4ab5073c3deba892f9119b529f255b7de63626
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2020-07-22 10:49:08 +1200

    4. Instructions
    Random bit of code. DOSEN'T APPLY

--- 04_instructions.py
+++ 04_instructions.py
@@ -1,8 +1,4 @@
-instructions = input("Welcome to the Area Perimeter Calculator:"
-                     " here are the instructions below:"
-                     "1. Please enter what shape you will be creating"
-                     "2. Then input what numbers you are using"
-                     "3. ")#Instructions
+#Instructions
 
 def instructions():
     print("****** Welcome to the Area Perimeter Calculator ******")
