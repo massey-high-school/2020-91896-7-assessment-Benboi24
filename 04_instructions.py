967ec0be5042372f2d0c082e9dea3817650becc2
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2020-07-22 10:37:16 +1200

    4. Instructions
    They work great asks for the right thing. Top part is my previous attempt which failed.

--- 04_instructions.py
+++ 04_instructions.py
@@ -2,4 +2,24 @@ instructions = input("Welcome to the Area Perimeter Calculator:"
                      " here are the instructions below:"
                      "1. Please enter what shape you will be creating"
                      "2. Then input what numbers you are using"
-                     "3. ")
+                     "3. ")#Instructions
+
+def instructions():
+    print("****** Welcome to the Area Perimeter Calculator ******")
+
+    first_time = input("\nHave you used this program before? ")
+
+    if first_time == "yes":
+        return ""
+
+    print()
+    print("***** Instructions ******")
+    print()
+    print("This program will ask you for...")
+    print("- The name of the shape you want to chose")
+    print("- It will ask for the height, base or other sides depending on shape")
+    print()
+    print("The program will print out the area and perimeter for the shape you chose.")
+    print()
+
+instructions()
