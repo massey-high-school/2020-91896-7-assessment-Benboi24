c574a91fd5e0845469371deb8ba3c1447a5030fe
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2020-07-24 14:34:35 +1200

    13_Herons_Rule
    Does ask correctly if they have all three sides. But dosen't do the rest.

---
+++ 13_Herons_Rule.pg.py
@@ -0,0 +1,26 @@
+# Herons Rule
+# Asks user if they have three sides. If they do it asks for sides. If they don't ask base and height
+
+A = float(input('Have you got all three sides for your triangle? '))
+
+if response == ["yes", "y"]:
+    print(input)
+input("What is side A?"
+      "What is side B?"
+      "What is side C")
+        
+if response == ["no", "n"]:
+    print(input)
+    input("What is your height?"
+          "What is your base?")
+
+# Calculate s
+s = (a + b + c) / 2
+
+# Calculate the area using herons rule
+area = (s *(s - a) * (s - b) * (s - c))
+
+# Calculate the area using base times height
+area = height * base
+
+
